2556e71ecbec491fdfb3ce741fa764222171afbb
Andrii Prokofiev <andrii_prokofiev1@epam.com>
2019-09-17 19:41:45 +0200

    Added and configured webdriverManager, deleted browser drivers

--- src/test/java/pageobject_model/driver/DriverSingleton.java
+++ src/test/java/pageobject_model/driver/DriverSingleton.java
@@ -1,5 +1,6 @@
 package pageobject_model.driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -7,18 +8,20 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
 
-    private static final String RESOURCES_PATH = "src//test//resources";
     private static WebDriver driver;
+
+    private DriverSingleton(){}
 
     public static WebDriver getDriver() {
         if (driver == null) {
+          //Implement getting property from console
             switch (System.getProperty("browser")) {
                 case "firefox": {
-                    System.setProperty("webdriver.gecko.driver", RESOURCES_PATH + "geckodriver.exe");
+                    WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                 }
                 default: {
-                    System.setProperty("webdriver.chrome.driver", RESOURCES_PATH + "chromedriver.exe");
+                    WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                 }
             }
